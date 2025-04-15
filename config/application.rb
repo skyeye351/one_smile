98a29e9e4da93ccaa139f2238985878db84f0f3c
skyeye351 <takahashifumi@Fumi-Mac.local>
2025-04-15 16:32:26 +0900

    ヘッダーとフッターの配置

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Myapp
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+    config.eager_load_paths << Rails.root.join("lib")
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -23,5 +23,11 @@ module Myapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.skip_routes true
+      g.helper false
+      g.test_framework nil
+      g.system_tests nil
+    end
   end
 end
