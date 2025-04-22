6db686e1183cb871507939de80366367ea2087e6
sky eye <esky1330@gmail.com>
2025-04-22 14:54:14 +0900

    Revert "sorceryインストール"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :require_login
-
-  private
-
-  def not_authenticated
-    redirect_to login_path
-  end
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
 end
