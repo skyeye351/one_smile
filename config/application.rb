54b83f07b19d53252a0791eba8565b5638063789
skyeye351 <takahashifumi@Fumi-Mac.local>
2025-04-24 14:25:16 +0900

    掲示板作成機能追加

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,6 @@ module Myapp
   class Application < Rails::Application
     config.load_defaults 7.2
     config.eager_load_paths << Rails.root.join("lib")
-    config.autoload_paths += %W(#{config.root}/app/decorators)
-    config.eager_load_paths += %W(#{config.root}/app/decorators)
 
     config.generators do |g|
       g.skip_routes true
