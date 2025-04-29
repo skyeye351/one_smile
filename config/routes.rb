78197a20d1fef7d3f7fc92cacb4f1a0d88b94040
skyeye351 <takahashifumi@Fumi-Mac.local>
2025-04-29 17:06:49 +0900

    掲示板の編集、削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "boards#index"
   resources :users, only: %i[new create]
   # ネストすることで関連するリソースの関係が明確になる。
-  resources :boards, only: %i[index new create show] do
+  resources :boards, only: %i[index new create show edit update destroy] do
     resources :comments, only: %i[create edit destroy], shallow: true
   end
 
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,8 +9,20 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      keyframes: {
+        'fade-in-up': {
+          '0%': {
+            opacity: 0,
+            transform: 'translateY(20px)',
+          },
+          '100%': {
+            opacity: 1,
+            transform: 'translateY(0)',
+          },
+        },
+      },
+      animation: {
+        'fade-in-up': 'fade-in-up 0.6s ease-out',
       },
     },
   },
