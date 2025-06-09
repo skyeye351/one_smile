00f85aebf6071b7f3c39ccbaf33a63349e51a6a2
skyeye351 <takahashifumi@Fumi-Mac.local>
2025-06-09 17:49:20 +0900

    掲示板更新失敗のエラーを修正＆画像のプレビュー表示

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
-import "@hotwired/turbo-rails"
-import "./controllers"
+import "@hotwired/turbo-rails";
+import "./controllers";
 
 document.addEventListener("turbo:load", () => {
   // ハンバーガーメニュー
---
+++ app/javascript/controllers/preview_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["input", "image"]
+
+  preview (){
+    const file = this.inputTarget.files[0]
+    if (!file) return
+
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      this.imageTarget.src = e.target.result
+    }
+    reader.readAsDataURL(file)
+  }
+}
