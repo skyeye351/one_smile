145cd3f704247e88dd1a8f8ce331888876e7fefc
skyeye351 <takahashifumi@Fumi-Mac.local>
2025-06-19 10:11:00 +0900

    未ログインで掲示板閲覧

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,5 @@
 class BoardsController < ApplicationController
-    skip_before_action :require_login, only: %i[index]
+    skip_before_action :require_login, only: %i[index show]
 
     def index
         @boards = Board.includes(:user)
