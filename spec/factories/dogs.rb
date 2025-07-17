4c109a749cfdf58738ec4a39a47527886c894d6b
skyeye351 <takahashifumi@Fumi-Mac.local>
2025-07-17 11:22:05 +0900

    Rspec:コメント削除

--- spec/factories/dogs.rb
+++ spec/factories/dogs.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :dog do
     user { nil }
-    name { "MyString" }
-    breed { "MyString" }
-    age { 1 }
-    place { "MyString" }
-    profile_image { "MyString" }
+    name { "Pochi" }
+    breed { "Shiba Inu" }
+    age { 3 }
+    place { "Tokyo" }
+    profile_image { "pochi.jpg" }
   end
 end
