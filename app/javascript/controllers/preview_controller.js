cffe25b2fdac851e0d813119cf39b50ecd080ca5
skyeye351 <takahashifumi@Fumi-Mac.local>
2025-08-01 16:21:12 +0900

    index, show, editページ作成

--- app/javascript/controllers/preview_controller.js
+++ app/javascript/controllers/preview_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["input", "image"]
 
-  preview (){
+  previewImage (){
     const file = this.inputTarget.files[0]
     if (!file) return
 
